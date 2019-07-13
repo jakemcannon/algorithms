feccadf81aadfc169f565bbe5c470599c55a191b
Jake Cannon <jakecan13@gmail.com>
2019-07-12 21:34:48 -0400

    number 14

---
+++ 0-99/14-longest-common-prefix.py
@@ -0,0 +1,26 @@
+
+# SOLUTION 1
+def longestCommonPrefix(self, strs: List[str]) -> str:
+    #step 1: error check for emmpy list
+    if not strs:
+        return ""
+    
+    #test everything on the shortest string
+    shortest_str = min(strs, key=len) # O(n)
+    lcp = ""
+    
+    index = 0
+    #use the shortest_str as the outer loop
+    for c in shortest_str:
+        # use != instead of == otherwise...
+        # you will catch some unwanted similar chars
+        for string in strs:
+            # this still checks shortest_str which isn't necessary
+            if string[index] != c:
+                return lcp
+        # Since we havn't hit a falsy char, keep building
+        # The != is what breaks us out on first false # condition
+        lcp += c
+        index += 1
+    
+    return lcp
