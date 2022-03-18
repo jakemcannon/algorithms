b7b10e858aad89d42e32d7bd843d810cd724f5ee
Jacob Cannon <jakecan13@gmail.com>
2022-03-17 20:34:12 -0400

    delete stale files

--- leetcode/200-299/283-move-zeroes.py
+++
@@ -1,9 +0,0 @@
-
-
-def moveZeroes(nums):
-    lag = 0
-    for i in range(len(nums)):
-        if nums[i] != 0:
-            if lag < i:
-                nums[lag], nums[i] = nums[i], nums[lag]
-            lag += 1
